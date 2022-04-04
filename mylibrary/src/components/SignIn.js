d0cb70f9580688ca777f49a21026d4e71f238508
Aniket-coder95 <kum.aniket@gmail.com>
2022-04-04 19:00:40 +0530

    email validation

--- mylibrary/src/components/SignIn.js
+++ mylibrary/src/components/SignIn.js
@@ -1,6 +1,27 @@
-import React from 'react'
+
 import { Link} from "react-router-dom";
+import React, { useState } from "react";
+import validator from 'validator';
+
+
  export default function SignIn(){
+
+    const [emailError, setEmailError] = useState('')
+  const validateEmail = (e) => {
+    var email = e.target.value
+  
+    if (validator.isEmail(email)) {
+        setEmailError('')
+    } else {
+      setEmailError('Enter valid Email!')
+    }
+  }
+    
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log("submit button clicked");
+        
+    }
      
      return(
         <div className="container py-5 h-100">
@@ -13,7 +34,11 @@ import { Link} from "react-router-dom";
 
                         <div className="form-outline mb-0">
                         <label className="form-label" htmlFor="typeEmailX-2">Email</label>
-                        <input type="email" id="typeEmailX-2" className="form-control form-control-m" />
+                        <input type="email" id="typeEmailX-2" className="form-control form-control-m"  onChange={(e)=> validateEmail(e)}/>
+                            <span style={{
+                            color: 'red',
+                            fontSize:15
+                            }}>{emailError}</span>
                         </div>
 
                         <div className="form-outline mb-0">
@@ -23,10 +48,12 @@ import { Link} from "react-router-dom";
 
                         <div className="form-outline mb-3">
                         <label className="form-label" htmlFor="typePasswordX-2">Role</label>
-                        <input type="text" id="typeEmailX-2" className="form-control form-control-m" />
+                        <select  className="form-control form-control-m" id="typeRoleX-2">
+                        <option>Student</option>
+                        <option value="">Admin</option> </select>
                         </div>
 
-                        <button className="btn btn-primary btn-lg btn-block" type="submit">Login</button>
+                        <button className="btn btn-primary btn-lg btn-block" type="submit" onClick={handleSubmit}>Login</button>
                         
                         <Link to ='/register'>
                             <button className="btn btn-secondary btn-lg btn-block" >Register</button>
